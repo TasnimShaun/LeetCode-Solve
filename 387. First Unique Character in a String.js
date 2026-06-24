eb66142f9a7cb7047c7fee6891e8626c35b467e0
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-06-24 14:56:58 +0800

    Create 387. First Unique Character in a String.js

---
+++ 387. First Unique Character in a String.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+
+    const freq = {};
+
+    // Count frequency of each character
+    for (const ch of s) {
+        freq[ch] = (freq[ch] || 0) + 1;
+    }
+
+    // Find first unique character
+    for (let i = 0; i < s.length; i++) {
+
+        if (freq[s[i]] === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
