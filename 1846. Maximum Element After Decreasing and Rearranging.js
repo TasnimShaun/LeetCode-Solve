849092941a4027e02371bc15e392ab78dc969aa4
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-06-28 23:21:57 +0800

    Create 1846. Maximum Element After Decreasing and Rearranging.js

---
+++ 1846. Maximum Element After Decreasing and Rearranging.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(
+    numbers
+) {
+
+    // Sort the array in ascending order
+    numbers.sort(
+        (firstValue, secondValue) =>
+            firstValue - secondValue
+    );
+
+    // The first element must be 1
+    numbers[0] = 1;
+
+    // Make sure the difference between
+    // adjacent elements is at most 1
+    for (
+        let index = 1;
+        index < numbers.length;
+        index++
+    ) {
+
+        numbers[index] = Math.min(
+            numbers[index],
+            numbers[index - 1] + 1
+        );
+    }
+
+    // The last element is the maximum
+    return numbers[numbers.length - 1];
+};
