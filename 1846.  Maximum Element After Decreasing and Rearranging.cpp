c7bf7002fbc757f45d6ddbe6b4755c9b296c1203
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-06-28 23:20:16 +0800

    Create 1846.  Maximum Element After Decreasing and Rearranging.cpp

---
+++ 1846.  Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(
+        vector<int>& numbers
+    ) {
+
+        // Sort the array in ascending order
+        sort(
+            numbers.begin(),
+            numbers.end()
+        );
+
+        // The first element must be 1
+        numbers[0] = 1;
+
+        // Make sure the difference between
+        // adjacent elements is at most 1
+        for (int index = 1;
+             index < numbers.size();
+             index++) {
+
+            numbers[index] = min(
+                numbers[index],
+                numbers[index - 1] + 1
+            );
+        }
+
+        // The last element is the maximum
+        return numbers.back();
+    }
+};
