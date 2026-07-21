2e77aefb5333d604669fc49dd59ada5238367016
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-21 11:32:32 +0800

    Create 88. Merge Sorted Array.cpp

---
+++ Array/Easy Difficulty/88. Merge Sorted Array.cpp
@@ -0,0 +1,85 @@
+/*
+=========================================================
+# 88. Merge Sorted Array
+
+## Problem
+You are given two integer arrays `nums1` and `nums2`, sorted in non-decreasing order, and two integers `m` and `n`, representing the number of elements in `nums1` and `nums2` respectively.
+
+Merge `nums1` and `nums2` into a single array sorted in non-decreasing order.
+
+The final sorted array should be stored inside `nums1`.
+
+---
+
+### Example 1
+
+Input:
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+Output:
+[1,2,2,3,5,6]
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+---
+
+Approach:
+1. Start from the end of both arrays.
+2. Compare nums1[m-1] and nums2[n-1].
+3. Place the larger element at nums1[last_index].
+4. Move the corresponding pointer.
+5. Copy remaining elements from nums2.
+
+Time Complexity: O(m + n)
+Space Complexity: O(1)
+
+=========================================================
+*/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        int last_index = m + n-1;
+        // merge reverse order 
+        while(m > 0 && n > 0 )
+        {
+            if(nums1[m-1]  > nums2[n-1])
+            {
+                nums1[last_index] = nums1[m-1];
+                m--;
+            }
+            else
+            {
+                nums1[last_index] = nums2[n-1];
+                n--;
+            }
+             last_index-- ;
+        }
+         // fill nums1 with leftover nums2  elements 
+
+         while( n > 0)
+         {
+            nums1[last_index] = nums2[n-1];
+            n--;
+            last_index--;
+         }
+    }
+};
