c858aca1dff04f2782be5611fe524d14f2f412d0
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-27 21:11:02 +0800

    Create 867. Transpose Matrix.cpp

---
+++ Array/Easy Difficulty/867. Transpose Matrix.cpp
@@ -0,0 +1,35 @@
+/*
+Question:Given a 2D integer array matrix, return the transpose of matrix.
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+  
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+*/
+
+// time complexity O(m × n)
+// space complexity O(m × n)
+  
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int column = matrix[0].size();
+
+        vector<vector<int>> transpose(column, vector<int>(row));
+        for( int i = 0;i < row;i++)
+        {
+            for(int j = 0; j <column;j++)
+            {
+               transpose[j][i] =  matrix[i][j];
+            }
+        }
+        return transpose;
+    } 
+};
